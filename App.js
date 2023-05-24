9a97212204fd06e22f60b900e6d5e964997fdb7f
Inna Pokydko <inna.pokydko@gmail.com>
2023-05-24 13:25:22 +0300

    app

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 // import { useFonts } from 'expo-font';
 
+const image = {uri: 'https://reactjs.org/logo-og.png'};
 export default function App() {
   // const [fontsLoaded] = useFonts({
   //   'Inter-Black': require('./assets/fonts/'),
@@ -12,9 +13,11 @@ export default function App() {
   // }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+       <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+       <Text style={styles.title}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+       </ImageBackground>
+          </View>
   );
 }
 
@@ -25,5 +28,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  title: {
+    marginTop: 16,
+    paddingVertical: 8,
+    borderWidth: 4,
+    borderColor: "#20232a",
+    borderRadius: 6,
+    backgroundColor: "#61dafb",
+    color: "#20232a",
+    textAlign: "center",
+    fontSize: 30,
+    fontWeight: "bold"
+  }
 });
 // style={{ fontFamily: 'Inter-Black', fontSize: 30 }}
