378091c9e91788c29a1884052cfaea4187b84fd0
Inna Pokydko <inna.pokydko@gmail.com>
2023-05-27 20:50:17 +0300

    update app

--- App.js
+++ App.js
@@ -26,14 +26,6 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
 
 // import 'react-native-gesture-handler';
 // import React from "react";
